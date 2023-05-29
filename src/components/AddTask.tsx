5d930e898a0fbabac45807670604c19289bd96a9
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-05-29 12:32:04 -0300

    Criação do componente input AddTask

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,10 +1,17 @@
 import styles from "./AddTask.module.css";
+import { PlusCircle } from "phosphor-react";
+
 export function AddTask() {
   return (
     <>
       <div className={styles.addTask}>
-        <input />
-        <button>Criar</button>
+        <input placeholder="Adicione um nova tarefa" />
+        <div className={styles.createButton}>
+          <span>Criar</span>
+          <span className={styles.icon}>
+            <PlusCircle size={20} />
+          </span>
+        </div>
       </div>
     </>
   );
