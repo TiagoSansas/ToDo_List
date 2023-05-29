a634461c921c3952d182e4793777720d4cf5b41e
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-05-29 10:55:24 -0300

    Adiciando cores padrões no projeto e criação do Header e addTask

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { AddTask } from "./components/AddTask";
+import { Header } from "./components/Header";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <AddTask />
+      </div>
+
+      <h1>Ola Mundo</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AddTask.tsx
@@ -0,0 +1,11 @@
+import styles from "./AddTask.module.css";
+export function AddTask() {
+  return (
+    <>
+      <div className={styles.addTask}>
+        <input />
+        <button>Criar</button>
+      </div>
+    </>
+  );
+}
